b29e6ac55fdfb45b9e79b0522b7897291d94f92d
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-12 00:40:14 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <a href="/target">Go to repro page</a>
+}
---
+++ app/target/page.tsx
@@ -0,0 +1,5 @@
+'use client'
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <p>should not be visible</p>
+}
